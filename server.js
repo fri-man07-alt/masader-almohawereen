6d2486b160517d5f8cf75fe8f16e02a935348356
fri-man07-alt <fri-man07@hotmail.com>
2025-08-13 18:13:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const express = require('express');
-const http = require('http');
 const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketIO(server);
 
-// خدمة الملفات الثابتة من نفس المجلد
-app.use(express.static(__dirname));
+const OWNER_PASS = "Aboadam94"; // كلمة سر المالك
+const ADMIN_PASSES = ["admin123", "mod123", "super456"]; // كلمات سر المشرفين
 
-// مسار الصفحة الرئيسية
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// تشغيل الخادم على البورت المخصص من Render
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
